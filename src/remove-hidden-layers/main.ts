b4e60542a0f1c1e3afa52f18d454b0f399efed48
Alex Guerrero <hola@alexguerrero.es>
2023-03-26 03:01:16 +0200

    Fix bug in remove hidden layers command

--- src/remove-hidden-layers/main.ts
+++ src/remove-hidden-layers/main.ts
@@ -1,40 +1,48 @@
-let totalModifiedElements = 0
+const ignoreChildrensOf = ['COMPONENT_SET', 'COMPONENT', 'INSTANCE']
+
+let totalModifiedLayers = 0
 
 function removeHiddenELements(node: SceneNode) {
   const current = node
 
   if ('visible' in current && !current.visible) {
-    current.remove()
-    totalModifiedElements += 1
+    try {
+      current.remove()
+    } catch (err) {
+      console.error(
+        `Error removing element with ID: ${current.id} and name: ${current.name}\n${err}`
+      )
+    }
+    totalModifiedLayers += 1
     return
   }
 
-  if ('children' in current) {
+  if (!ignoreChildrensOf.includes(current.type) && 'children' in current) {
     current.children.forEach((children: SceneNode) => {
       removeHiddenELements(children)
     })
   }
 }
 
-function getMessage(target: 'file' | 'selection', totalModifiedElements: number) {
-  const elementsCopy = totalModifiedElements > 1 ? 'elements' : 'element'
+function getMessage(target: 'file' | 'selection', modifiedLayers: number) {
+  const elementsCopy = modifiedLayers > 1 ? 'elements' : 'element'
 
-  if (totalModifiedElements === 0) {
+  if (modifiedLayers === 0) {
     return `No hidden ${elementsCopy} has been found in current ${target}`
   }
 
-  return `Removed ${totalModifiedElements} hidden ${elementsCopy} from current ${target}`
+  return `Removed ${modifiedLayers} hidden ${elementsCopy} from current ${target}`
 }
 
 export default function () {
   const selectionIsEmpty = figma.currentPage.selection.length === 0
   const target = selectionIsEmpty ? 'file' : 'selection'
   const targetElements = selectionIsEmpty
-    ? figma.currentPage.selection
-    : figma.root.children.flatMap(page => page.children)
+    ? figma.root.children.flatMap(page => page.children)
+    : figma.currentPage.selection
 
   targetElements.forEach(removeHiddenELements)
 
-  figma.notify(getMessage(target, totalModifiedElements))
+  figma.notify(getMessage(target, totalModifiedLayers))
   figma.closePlugin()
 }
